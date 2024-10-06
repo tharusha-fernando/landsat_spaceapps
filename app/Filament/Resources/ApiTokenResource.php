1ee51c9486d9faefad2a6a306dbd1f980b375ca0
Tharusha <tharushafdo2000@gmail.com>
2024-10-06 10:14:52 +0530

    Actions Updated

--- app/Filament/Resources/ApiTokenResource.php
+++ app/Filament/Resources/ApiTokenResource.php
@@ -53,6 +53,7 @@ class ApiTokenResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/LocationResource.php
+++ app/Filament/Resources/LocationResource.php
@@ -98,6 +98,8 @@ class LocationResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
